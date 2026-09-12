c6f0d078602c645e1fcef048b246ddb57315d715
Nikita Samodurov <nikita98351@gmail.com>
2026-09-12 17:36:15 +0300

    test: improve tests for labeled two cycles graphs

--- project/library/graphs.py
+++ project/library/graphs.py
@@ -4,6 +4,16 @@ from typing import Any, Iterable, Tuple, Union, Set
 import networkx as nx
 
 
+def get_unique_labels(graph: nx.MultiDiGraph) -> Set[str]:
+    unique_labels: Set[str] = set()
+    for _u, _v, data in graph.edges(data=True):
+        label = data.get("label")
+        if label is not None:
+            unique_labels.add(label)
+
+    return unique_labels
+
+
 def graph_info_from_name(name: str) -> Tuple[int, int, Set[str]]:
     path = cfpq_data.download(name)
     return graph_info_from_path(path)
@@ -15,11 +25,7 @@ def graph_info_from_path(path: Union[str, Path]) -> Tuple[int, int, Set[str]]:
     num_vertices: int = graph.number_of_nodes()
     num_edges: int = graph.number_of_edges()
 
-    unique_labels: Set[str] = set()
-    for _u, _v, data in graph.edges(data=True):
-        label = data.get("label")
-        if label is not None:
-            unique_labels.add(label)
+    unique_labels: Set[str] = get_unique_labels(graph)
 
     return num_vertices, num_edges, unique_labels
 
